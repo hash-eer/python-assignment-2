916beae75774b5e38728c822263c30c420e3f2d2
Hasheer <mohammedhasheer49@gmail.com>
2022-09-07 11:16:37 +0530

    8th and 10th prog is changed

--- python assignment 2 strings/prog8.py
+++ python assignment 2 strings/prog8.py
@@ -1,7 +1,14 @@
 #  Write a Python function that takes a list of words and returns the length of the longest one. 
 
-str=input("enetr the strings to find the largest word")
-list=str.split()
+# str=input("enetr the strings to find the largest word")
+# list=str.split()
+
+list=[]
+n=int(input("enetr the size of the list"))
+for i in range(0,n):
+    print("enter the words at index ",i)
+    items=input()
+    list.append(items)
 print(list)
 large=0
 word=''
